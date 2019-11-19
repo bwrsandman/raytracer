e6da70c4e493e544cc5a4dd8dfa41e99d35eb5f1
Sandy Carter <bwrsandman@gmail.com>
2019-11-19 16:47:58 +0100

    game: Add emscripten web-assembly cross compilation

--- src/input.cpp
+++ src/input.cpp
@@ -1,32 +1,47 @@
 #include "input.h"
 #include <SDL.h>
 
-Input::Input():quit(false) {}
+Input::Input()
+  : quit(false)
+{}
+
 Input::~Input() = default;
 
+void
+Input::run()
+{
+  SDL_Event event;
 
-void Input::run(){
-	SDL_Event event;
-
-	while (SDL_PollEvent(&event)) {
-		switch (event.type) {
-		case SDL_QUIT:
-			quit = true;
-			break;
-		case SDL_KEYDOWN:
-			switch (event.key.keysym.sym) {
-			case SDLK_ESCAPE:
-				quit = true;
-				break;
-			}
-			break;
-		}
-	}
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+      case SDL_QUIT:
+        quit = true;
+        break;
+      case SDL_KEYDOWN:
+        switch (event.key.keysym.sym) {
+#if __EMSCRIPTEN__
+          case SDL_SCANCODE_ESCAPE: // weird sdl2 emscripten bug
+#else
+          case SDLK_ESCAPE:
+#endif
+            quit = true;
+            break;
+        }
+        break;
+    }
+  }
 }
 
-void Input::update_camera(Camera& camera) const{}
-void Input::update_ui(Ui& ui) const {}
+void
+Input::update_camera(Camera& camera) const
+{}
 
-bool Input::should_quit() const {
-	return quit;
+void
+Input::update_ui(Ui& ui) const
+{}
+
+bool
+Input::should_quit() const
+{
+  return quit;
 }
